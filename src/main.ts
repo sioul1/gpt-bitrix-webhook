6ae4c3cdef59ba16630c52caaeab772d711e2944
werbm <werbm.silva.grupowra@outlook.com>
2025-06-19 09:13:19 -0300

    add: validation pipe to maints

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
   console.log('server running on the port: ', process.env.PORT);
 }
 bootstrap();
